e0a6b721f20d87779138c86979bf0ca1143512e6
majdbawab <majdbawab@yahoo.com>
2025-12-07 16:56:44 +0400

    Implement flight data API handler

---
+++ api/api/index.js
@@ -0,0 +1,16 @@
+import fetch from "node-fetch";
+
+export default async function handler(req, res) {
+    const API_KEY = "f76cc7a734cbd9daae6d9ec1e653f60a";
+    const airport = req.query.airport || "DXB";
+
+    const url = `http://api.aviationstack.com/v1/flights?access_key=${API_KEY}&dep_iata=${airport}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.toString() });
+    }
+}
